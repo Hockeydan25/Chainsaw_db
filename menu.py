9f49f21e0735ac4db6d7a3529dbd33e790ec614a
Dan Smestad <hockeydan25@gmail.com>
2022-02-11 15:05:52 -0600

    changed delete_record to try and except that correct the if else issue. corrected my database creation and table. added drop table on quit. records will be generated but not the same.

--- menu.py
+++ menu.py
@@ -2,16 +2,16 @@
 Lab5 SQLite  Dan Smestad ITEC 2905-80 Software Dev. Capstone Clara James
 A menu - you need to add the database and fill in the functions. 
 """
+
 import sqlite3
+
 
 db = 'chainsaw_juggling_records_db.sqlite'
 
 
-class RecordError(Exception):
-    pass
-
 def main():
-    menu_text = """    
+    menu_text = """  
+  
     Chainsaw Juggling Record Holders as of July 2018
 
     1. Display all records
@@ -21,7 +21,9 @@ def main():
     5. Search by Name
     6. Quit
     """
-    
+    create_table()
+    insert_record_holders_data()
+
     while True:
         print(menu_text)
         choice = input('Enter your choice: ')
@@ -36,6 +38,7 @@ def main():
         elif choice == '5':
             search_records_by_name()    
         elif choice == '6':
+            drop_table_on_quit() 
             break
         else:
             print('Not a valid selection, please try again')
@@ -47,15 +50,21 @@ def create_table():
     conn.commit  #need to have:IF NOT EXISTS for tables and DB's.
     conn.close()   
 
+def drop_table_on_quit():
+    with sqlite3.connect(db) as conn:  #deleting the table for jugglers records.
+        conn.execute('DROP TABLE jugglers')  
+    conn.commit  #need to have:IF NOT EXISTS for tables and DB's.
+    conn.close()   
 
 def insert_record_holders_data():
     with sqlite3.connect(db) as conn:
-    #THIS WILL keep creating if you re-reun
+
+    #THIS WILL keep creating if you re-reun.option on quit deletes table
         conn.execute('INSERT INTO jugglers VALUES ("Janne Mustonen", "Finland", "98")' )
         conn.execute('INSERT INTO jugglers VALUES ("Ian Stewart", "Canada", 94)')
         conn.execute('INSERT INTO jugglers VALUES ("Aaron Greg", "Canada", "88")' )
         conn.execute('INSERT INTO jugglers VALUES ("Chad Taylor", "USA", 78)')
-
+    
     conn.close()  
 
          
@@ -75,9 +84,9 @@ def search_records_by_name():
     results = conn.execute('SELECT * FROM jugglers WHERE name like ?', (search_cases, ))
     first_row = results.fetchone()
     if first_row:
-        print('Your jugglers record is: ', first_row)
+        message('\nYour jugglers record is: ', first_row)
     else:
-        print('not found in database')     
+        message('\nnot found in database')     
     conn.close()
 
 
@@ -104,17 +113,25 @@ def edit_existing_record():
 #'todo delete existing record. What if user wants to delete record that does not exist?'
 def delete_record():
     delete_name = input('enter jugglers name to delete: ')
-    delete_cases = delete_name.upper() or delete_name.lower()        
+    delete_cases = delete_name.lower()     
     with sqlite3.connect(db) as conn:
-        if delete_cases:    
-            for row in conn.execute('SELECT * FROM jugglers WHERE name like ?', (delete_cases, )):
+        try:                 
+            for row in conn.execute('SELECT * FROM jugglers WHERE lower(name) like ?', (delete_cases, )):
                 conn.execute('DELETE FROM jugglers WHERE name = ? ', (row[0],  ))
                 print('Your juggler:', delete_cases, 'was deleted, please use menu to verify.')           
-        else:       
-            print('not found in database')     
+        except:                   
+               print('not found in database')     
         #conn.execute('DELETE FROM jugglers WHERE name = ?', (delete_cases,  ))
         #currently only exact match deletes record, update with any case entery sqlite does care case.   
     conn.close()
+
+def message(msg):
+    """ Prints a message for the user
+    :param msg: the message to print"""
+    print(msg)
+
+class RecordError(Exception):
+    pass
 
    
 if __name__ == '__main__':
