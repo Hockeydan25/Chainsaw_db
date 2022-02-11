1212040d5e55d1c3e69d9da5e6099ba5539232e3
Dan Smestad <hockeydan25@gmail.com>
2022-02-11 09:19:41 -0600

    moved call to create db into main. added record holders data to db.

--- menu.py
+++ menu.py
@@ -6,11 +6,6 @@ import sqlite3
 
 db = 'chainsaw_juggling_records_db.sqlite'
 
-def create_table():
-    with sqlite3.connect(db) as conn:  #creating the table for jugglers records.
-        conn.execute('CREATE TABLE IF NOT EXISTS jugglers (name text, country text, catch_count int)')  
-    #need to have:IF NOT EXISTS for tables and DB's.
-    conn.close()   
 
 def main():
     menu_text = """    
@@ -22,7 +17,8 @@ def main():
     4. Delete record 
     5. Quit
     """
-
+    create_table()
+    
     while True:
         print(menu_text)
         choice = input('Enter your choice: ')
@@ -41,6 +37,23 @@ def main():
         else:
             print('Not a valid selection, please try again')
 
+
+def create_table():
+    with sqlite3.connect(db) as conn:  #creating the table for jugglers records.
+        conn.execute('CREATE TABLE IF NOT EXISTS jugglers (name text, country text, catch_count int)')  
+    #need to have:IF NOT EXISTS for tables and DB's.
+    conn.close()   
+
+
+# def insert_record_holders_data():
+#     with sqlite3.connect(db) as conn:
+#     #THIS WILL keep creating if you re-reun
+#         conn.execute('INSERT INTO jugglers VALUES ("Janne Mustonen", "Finland", "98")' )
+#         conn.execute('INSERT INTO jugglers VALUES ("Ian Stewart", "Canada", 94)')
+#         conn.execute('INSERT INTO jugglers VALUES ("Aaron Greg", "Canada", "88")' )
+#         conn.execute('INSERT INTO jugglers VALUES ("Chad Taylor", "USA", 78)')
+#     conn.close()  
+
          
 def display_all_records():
     conn = sqlite3.connect(db)  # connects datbase link
@@ -57,14 +70,14 @@ def add_new_record():
     new_catch_count = int(input('enter number of catches: '))
     # no format strings {new_id} use paramerized value queries, sql statement.
     with sqlite3.connect(db) as conn:
-        conn.execute(f'INSERT INTO jugglers VALUES (?, ?, ?)', (new_name, new_country, new_catch_count) )
+        conn.execute('INSERT INTO jugglers VALUES (?, ?, ?)', (new_name, new_country, new_catch_count) )
     conn.close()
 
 #'todo edit existing record. What if user wants to edit record that does not exist?'
 def edit_existing_record():
-    conn = sqlite3.connect(db)
-    edit_name = 'Dan'
-    edit_catch_count = '96'
+    #conn = sqlite3.connect(db)
+    edit_name = input('enter jugglers name to edit: ')
+    edit_catch_count = int(input('enter new number of catches: '))
     with sqlite3.connect(db) as conn:
         conn.execute('UPDATE jugglers SET name = ? WHERE catch_count = ?', (edit_name, edit_catch_count) )
     conn.close()
@@ -72,6 +85,7 @@ def edit_existing_record():
 
 #'todo delete existing record. What if user wants to delete record that does not exist?'
 def delete_record(jugglers_name):
+    jugglers_name = '' 
     with sqlite3.connect(db) as conn:
         conn.execute('DELETE FROM jugglers WHERE name = ?', (jugglers_name, ) )
     # does it matter the order here?
